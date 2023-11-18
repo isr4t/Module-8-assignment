ab970c40f0e1a4a043b661f00bb36dc6f4be68e2
Israt Jahan <79652269+isratbd24@users.noreply.github.com>
2023-11-18 21:39:23 +0600

    Update ProfileController.php

--- ProfileController.php
+++ ProfileController.php
@@ -2,9 +2,32 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
-
 class ProfileController extends Controller
 {
-    //
+    public function index($id)
+    {
+        $name = "Donald Trump";
+        $age = "75";
+
+        
+        $data = [
+            'id' => $id,
+            'name' => $name,
+            'age' => $age,
+        ];
+
+        
+        $cookieName = 'access_token';
+        $cookieValue = '123-XYZ';
+        $minutes = 1;
+        $path = '/';
+        $domain = $_SERVER['SERVER_NAME'];
+        $secure = false;
+        $httpOnly = true;
+
+        
+        \Illuminate\Support\Facades\Cookie::make($cookieName, $cookieValue, $minutes, $path, $domain, $secure, $httpOnly);
+
+        return response()->json($data)->cookie($cookieName, $cookieValue, $minutes, $path, $domain, $secure, $httpOnly);
+    }
 }
